c3346d120a633e2b3a0f5861c62e54255fbccb4b
jamilelima <jamilelima@protonmail.com>
2019-04-29 00:27:11 -0300

    initial commit - add all files

---
+++ src/components/sections/Header.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import styled from 'styled-components';
+import { StaticQuery, graphql } from 'gatsby';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+import Img from 'gatsby-image';
+import { Container } from '@components/global';
+
+const Header = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        art_build: file(
+          sourceInstanceName: { eq: "art" }
+          name: { eq: "build" }
+        ) {
+          childImageSharp {
+            fluid(maxWidth: 1400) {
+              ...GatsbyImageSharpFluid_withWebp_tracedSVG
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <HeaderWrapper>
+        <Container>
+          <Grid>
+            <Art>
+              <Img fluid={data.art_build.childImageSharp.fluid} />
+            </Art>
+            <Text>
+              <h1>
+                Transformando a caixa preta da Internet em uma caixa de ferramentas.
+              </h1>
+              <br />
+              <p>
+                <StyledAnchorLink href="#trilhas">
+                  Vamos começar? &nbsp;&#x2794;
+                </StyledAnchorLink>
+              </p>
+            </Text>
+          </Grid>
+        </Container>
+      </HeaderWrapper>
+    )}
+  />
+);
+
+const HeaderWrapper = styled.header`
+  background-color: ${props => props.theme.color.secondary};
+  padding-top: 96px;
+
+  @media (max-width: ${props => props.theme.screen.md}) {
+    padding-top: 128px;
+  }
+`;
+
+const Art = styled.figure`
+  width: 100%;
+  margin: 0;
+
+  > div {
+    width: 120%;
+    margin-bottom: -4.5%;
+
+    @media (max-width: ${props => props.theme.screen.md}) {
+      width: 100%;
+    }
+  }
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  grid-gap: 64px;
+
+  @media (max-width: ${props => props.theme.screen.md}) {
+    grid-template-columns: 1fr;
+    grid-gap: 80px;
+
+    > ${Art} {
+      order: 2;
+    }
+  }
+`;
+
+const Text = styled.div`
+  justify-self: center;
+
+  @media (max-width: ${props => props.theme.screen.md}) {
+    justify-self: start;
+  }
+`;
+
+const StyledAnchorLink = styled(AnchorLink)`
+  color: inherit;
+  text-decoration: none;
+
+  &:hover {
+    color: ${props => props.theme.color.black.regular};
+  }
+`;
+
+export default Header;
